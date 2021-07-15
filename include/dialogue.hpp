2199d5209f6435fb62f64047056a8ead11b7b5a7
rlatosky <latoskyr@gmail.com>
2021-07-14 22:34:45 -0400

    Inital commit on new branch

---
+++ include/dialogue.hpp
@@ -0,0 +1,15 @@
+//include stuff goes here
+
+class dialogue
+{
+private:
+    void createBox();
+public:
+    dialogue(/* args */);
+    ~dialogue();
+
+};
+
+void createBox() {
+    //Stuff goes here
+}
